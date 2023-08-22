e30d1c3c710a96c4926f418ef50496589e8501da
uturuncoglu <turuncu@ucar.edu>
2023-08-22 17:32:09 -0600

    initial implementation for cdeps inline capability

--- cmake/configure_apps.cmake
+++ cmake/configure_apps.cmake
@@ -34,6 +34,7 @@ if(APP MATCHES "^(ATM|ATMW|ATMWM|ATMAQ|ATML)$")
   else()
     message("Configuring UFS app in Atmosphere Only mode")
   endif()
+  set(CDEPS      ON  CACHE BOOL "Enable CDEPS"               FORCE)
 endif()
 
 if(APP MATCHES "^(NG-GODAS)$")
